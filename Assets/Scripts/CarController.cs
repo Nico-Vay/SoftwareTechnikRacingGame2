3b615ae53b9d09ea98bd055a41dc20a788295985
Nico-Vay <darkolli2013@gmail.com>
2022-03-08 13:07:21 +0100

    Asset Changes, Python Changes

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody sphereRB;
+
+    private float moveInput;
+    private float turnInput;
+
+    public float forwardSpeed;
+    public float reverseSpeed;
+    public float turnSpeed;
+
     void Start()
     {
-        
+        //detach Motor from Car
+        sphereRB.transform.parent = null;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+
+        moveInput *= moveInput > 0 ? forwardSpeed : reverseSpeed;
+
+        transform.position = sphereRB.transform.position;
+
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.Rotate(0, newRotation, 0, Space.World);
+
+    }
+
+    private void FixedUpdate()
+    {
+        sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
     }
 }
